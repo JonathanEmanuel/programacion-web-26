eb78f77191a0593b0e1d0d7dade4122527b8db46
Jonathan Cruz <cruzjonathan.e@gmail.com>
2026-05-28 18:44:03 -0300

    Semana 08 - DOM

---
+++ semana_08/app.js
@@ -0,0 +1,22 @@
+import { products} from './data.js'
+
+const d = document;
+const titulo = d.querySelector('h1');
+const cantidad = d.querySelector('span');
+const contenedorProductos = d.querySelector('#contenedorProductos');
+cantidad.textContent = products.length;
+
+console.log( products.length);
+
+
+for (let index = 0; index < products.length; index++) {
+    const product = products[index];
+    console.log(product);
+    contenedorProductos.innerHTML += 
+        ` <div class="card">
+                <img src="${product.img}" alt="${product.name}">
+                <h3> ${ product.name }</h3>
+                <h4>$ ${ product.price}</h4>
+            </div>`;
+}
+
---
+++ semana_08/data.js
@@ -0,0 +1,8 @@
+export const products = [
+    { id:1, name: 'Monitor FULL HD', price: 320, img: 'images/monitor.png' },
+    { id:2, name: 'Teclado Mec 1', price: 120, img: 'images/teclado.png' },
+    { id:3, name: 'Teclado Mec 2', price: 170, img: 'images/teclado.png' },
+    { id:4, name: 'Mouse', price: 150, img: 'images/mouse.png' },
+    { id:5, name: 'Mouse último', price: 150, img: 'images/mouse.png' }
+
+]
